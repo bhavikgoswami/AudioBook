34ac697ad96c72cfc045251d9f0392bbfe398b9e
hiral patel <hiral.patel201@gmail.com>
2022-05-26 00:43:48 +0530

    Nav added

---
+++ app/src/main/java/com/book/audiobook/ui/fragment/LibraryFragment.kt
@@ -0,0 +1,34 @@
+package com.book.audiobook.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.book.audiobook.R
+
+class LibraryFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment LibraryFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = LibraryFragment().apply {}
+    }
+}
