2e1abda242e41d23c3dcc7b6c9db8142354cf9b6
hiral patel <hiral.patel201@gmail.com>
2022-05-25 23:24:44 +0530

    Nav added

--- app/src/main/java/com/book/audiobook/ui/activity/MainActivity.kt
+++ app/src/main/java/com/book/audiobook/ui/activity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.book.audiobook.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.book.audiobook.R
+import com.book.audiobook.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: MainActivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
