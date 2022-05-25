987036eb217cc915fd4aeec2e4f691a1960b9e44
Bhavik Goswami <bhavik.3mindsdigital@gmail.com>
2022-05-25 22:43:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/book/audiobook/app/AudioBookApplication.java
@@ -0,0 +1,21 @@
+package com.book.audiobook.app;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+import io.paperdb.Paper;
+
+/**
+ * This is application class which initialize all the required modules, libraries,etc
+ */
+@HiltAndroidApp
+public class AudioBookApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Paper.init(getApplicationContext());
+
+
+    }
+}
