c2ffb07026730fe99b0a64615e27aa05d23e9724
hiral patel <hiral.patel201@gmail.com>
2022-05-27 00:00:43 +0530

    UI set data set

--- app/src/main/java/com/book/audiobook/ui/activity/MainActivity.kt
+++ app/src/main/java/com/book/audiobook/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.book.audiobook.ui.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.book.audiobook.R
 import com.book.audiobook.databinding.MainActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,22 +24,27 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-       val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        setupWithNavController(binding.bottomNavigationView, navController)
+//        setupWithNavController(binding.bottomNavigationView, navController)
         //NavigationUI.setupWithNavController( navController)
-        /*navController.addOnDestinationChangedListener { _, destination, bundle ->
+
+        navController.addOnDestinationChangedListener { _, destination, bundle ->
             when (destination.id) {
                 R.id.homeFragment,
                 R.id.searchFragment,
                 R.id.libraryFragment,
                 -> {
-                    R.id.homeFragment
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.GONE
                 }
             }
         }
 
         binding.bottomNavigationView.setupWithNavController(navController)
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)*/
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
